033270696e0905b0090bda35569a21c8cd9b1bf9
Devman <leonardopachecoduran@gmail.com>
2020-12-15 20:04:59 -0300

    Version nueva
    arroja null en los int pero llama los campos correctamente

--- Zoologico/src/Main.java
+++ Zoologico/src/Main.java
@@ -5,24 +5,44 @@ public class Main extends Gato {
     {
         crearGato("micifus");
 
+        creaUnGato();
+        creaOtroGato();
+        creaTercerGato();
+        creaCuartoGato();
     }
 
     public static String crearGato(String nombreGato)
     {
-
         nombreGato = "Charlie";
-
         System.out.println(" Hola");
         System.out.println(" Soy " + nombreGato);
         return nombreGato;
 
-
     }
 
+
+public static void creaUnGato(){
     Gato gato = new Gato ("cachorrito","angora" , "negro" , 3);
-//  Gato gato1 = new Gato ("margarito","angora");
-//  Gato gato3 = new Gato ("sultan","feral");
-//Gato cat = new Gato ("sultan","negro", 5);
+    System.out.println(" hola yo soy " + gato.nombre + ", de raza " + gato.raza + ", color " + gato.color + ", y " + gato.edad + "años" );
+
+}
+
+    public static void creaOtroGato() {
+        Gato gatito  = new Gato ( "gatochico" , "gatonuevo", "oscurito" , 11);
+        System.out.println("el segundo gato es " + gatito.nombre + "y es de raza " + gatito.raza);
+    }
+
+    public static void creaTercerGato() {
+        Gato gatonio  = new Gato ( "felini" , "gatomediano" , "clarito " , 2);
+        System.out.println("el tercer gato es " + gatonio.nombre + "y tiene " +  gatonio.edad+ " años ");
+
+    }
+    public static void creaCuartoGato() {
+        Gato gatinio  = new Gato ( "felini" , "gatomediano" , "clarito " , 2);
+        System.out.println("el tercer gato es " + gatinio.nombre + "y tiene " +  gatinio.edad+ " años ");
+        System.out.println(gatinio.edad);
+
+    }
 
 }
 
