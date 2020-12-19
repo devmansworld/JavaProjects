f7533332000d005a3e67e1843b4dd2ff02e6d1f4
Devman <leonardopachecoduran@gmail.com>
2020-12-18 22:21:13 -0300

    Add files via upload

---
+++ zoologicocuatro/src/Gato.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.lang.String;
+
+public class Gato {
+
+    private String nombre;
+    private String raza;
+    private String color;
+    private  Integer edad;
+
+    public Gato(String nombre, String raza, String color, Integer edad)
+    {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.edad = edad;
+    }
+
+    public Gato() {}
+
+    public Gato(String nombre, String raza)
+    { this.nombre = nombre;
+        this.raza = raza;}
+
+    // constructor con solo 2 parametros
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+}
